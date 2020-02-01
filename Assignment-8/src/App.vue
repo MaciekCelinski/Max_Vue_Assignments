46aad267a6b5310289cb2df2dc8f9ff97ca8df15
MaciekCelinski <mac.celinski@gmail.com>
2020-02-01 23:27:51 +0100

    FINISH

--- Assignment-8/src/App.vue
+++ Assignment-8/src/App.vue
@@ -3,13 +3,12 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button @click="showTemplate='app-blue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="showTemplate='app-green'" class="btn btn-success">Load Green Template</button>
+                <button @click="showTemplate='app-red'" class="btn btn-danger">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+
+                <component :is="showTemplate">{{showText}}</component>
             </div>
         </div>
     </div>
@@ -21,6 +20,12 @@
     import Red from './components/Red.vue';
 
     export default {
+        data(){
+            return {
+                showText: 'Say Hello',
+                showTemplate: 'app-blue'
+            }
+        },
         components: {
             appBlue: Blue,
             appGreen: Green,
