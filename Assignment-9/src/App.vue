b45514e2a77f9ed2ea4cb70da8609c0d2f9591fb
MaciekCelinski <mac.celinski@gmail.com>
2020-02-13 21:51:19 +0100

    FINISH

--- Assignment-9/src/App.vue
+++ Assignment-9/src/App.vue
@@ -1,46 +1,81 @@
 <template>
-    <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
+	<div class="container">
+		<form>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+					<form>
+                        <FullName v-model="fullName"/>
+						<div class="form-group">
+							<label for="email">Email address</label>
+							<input v-model.once="email" type="email" class="form-control" id="email"
+							       aria-describedby="emailHelp" placeholder="Enter email">
+							<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+								else.</small>
+						</div>
+						<div class="form-group">
+							<label for="password">Password</label>
+							<input v-model="password" type="password" class="form-control" id="password"
+							       placeholder="Password">
+						</div>
 
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
-                </div>
-            </div>
-        </form>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" value="yes" v-model="storeData">Yes
+                            </label>
+                            <label>
+                                <input type="radio" value="no" v-model="storeData">No
+                            </label>
+                        </div>
+
+<!--						<div class="form-check">-->
+<!--							<input v-model="storeData" type="checkbox" class="form-check-input" id="storeData">-->
+<!--							<label class="form-check-label" for="storeData">Store Data</label>-->
+<!--						</div>-->
+						<button @click.prevent="submitData()" type="submit" class="btn btn-primary">Submit</button>
+					</form>
+				</div>
+			</div>
+		</form>
+		<hr>
+		<div v-if="showData" class="row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>Your Data</h4>
+					</div>
+					<div class="panel-body">
+						<p>Full Name: {{fullName}}</p>
+						<p>Mail: {{email}}</p>
+						<p>Password: {{password}}</p>
+						<p>Store in Database?: {{storeData ? "Yes" : "No"}}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+    import FullName from "./FullName.vue";
     export default {
+        data() {
+            return {
+                fullName: 'Mac Cel',
+                email: "",
+                password: "",
+                storeData: "no",
+                showData: false
+            }
+        },
+        methods: {
+            submitData() {
+                this.storeData === "yes" ? this.showData = true : this.showData = false
+            }
+        },
+        components: {
+           FullName
+        }
     }
 </script>
 
---
+++ Assignment-9/src/FullName.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<div class="form-group">
+			<label for="firstName">First Name</label>
+			<input type="text" class="form-control" id="firstName"
+			       :value="firstName" aria-describedby="nameHelp" placeholder="Enter Fist Name" @input="nameChanged(true, $event)">
+		</div>
+		<div class="form-group">
+			<label for="lastName">Last Name</label>
+			<input type="text" class="form-control" id="lastName"
+			       :value="lastName" aria-describedby="nameHelp" placeholder="Enter Last Name" @input="nameChanged(false, $event)">
+		</div>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: "FullName",
+        props:['value'],
+        data() {
+            return {
+                fullName: ""
+            }
+        },
+	    methods:{
+            nameChanged(isFirst, event){
+				let name = "";
+				if (isFirst) {
+				    name= event.target.value + " " + this.lastName
+				} else {
+                    name= this.firstName + " " + event.target.value
+				}
+				this.fullName = name
+				this.$emit('input', this.fullName)
+            }
+	    },
+	    computed: {
+            firstName() {
+                return this.value.split(" ")[0]
+            },
+		    lastName(){
+                return this.value.split(" ")[1]
+		    }
+	    }
+    }
+</script>
+
+<style scoped>
+
+</style>
