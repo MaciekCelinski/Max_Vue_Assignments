55279be0251264b417577ed5043197dd33976716
MaciekCelinski <mac.celinski@gmail.com>
2020-02-14 21:11:20 +0100

    FINISH

--- Assignment-10/src/App.vue
+++ Assignment-10/src/App.vue
@@ -5,14 +5,41 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <button v-myOn:click="clicked" class="btn btn-primary">Click Me</button>
+                <hr>
+                <div v-myOn:mouseleave="mouseLeft" style="width: 100px; height: 100px; background-color: lightcoral"></div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        directives: {
+            'myOn' : {
+                bind(el, binding){
+                   // el.onclick = ()=>{
+                   //     binding.value()
+                   // }
+                    // type is a type of event like 'click', 'keyUp','mouseLeave'
+                    const type = binding.arg;
+                    console.log(type)
+                    // fn is a reference to assigned function like 'clicked'
+                    const fn = binding.value;
+                    console.log(fn)
+                    el.addEventListener(type, fn)
+                }
+            }
+        },
+        methods: {
+            clicked(){
+                alert('I was clicked')
+            },
+            mouseLeft(){
+                alert('She really did')
+            }
+        }
+    }
 </script>
 
 <style>
